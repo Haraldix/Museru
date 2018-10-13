38ddefd1b62d09f5596c715467b6428fbbdd40f9
Harri Pohjosaho <Harri Pohjosaho@DESKTOP-FOQ9AUT>
2018-10-13 21:03:50 +0300

    fixed bugs, configparser changed python 3

--- Etyde001.py
+++ Etyde001.py
@@ -1,20 +1,32 @@
 import os
 import sys
+import getopt
+import configparser
 
-class Etyde:
+#color constants
+RED ="\033[0;31m"
+GREEN ="\033[0;32m"
+BLUE ="\033[0;34m"
+YELLOW ="\033[1;33m"
+NC ="\033[0m" #No color
 
 
-def help(self):
-      print "\n Usage: python etydes.py [seed] [-sphv]"
-      print "       -s, --seed        Seed "
-      print "       -p, --play        Play "  
-      print "       -h, --help        This help "
-      print "       -v, --verbose     Verbose"
+class Etyde:  
+  global AUX
+  etydes_ini = 'etydes.ini'
+  output_path = ''
+  
+  def help(self):
+      print ("\n Usage: python etydes.py [seed] [-sphv]")
+      print ("       -s, --seed        Seed ")
+      print ("       -p, --play        Play ")  
+      print ("       -h, --help        This help ")
+      print ("       -v, --verbose     Verbose")
       sys.exit(2)
       return None
     
   def parseOnlineArgs(self, argv):
-
+      
       #switches
       global MAKE #make autotest
       #show help if no arguments
@@ -22,24 +34,27 @@ def help(self):
         self.help()
 
       #online arguments
-
+    
       try:
         opt, args = getopt.getopt(sys.argv[1:],"-sphv",["seed="])
-      except getopt.GetoptError:
-        print '\nSyntax Error'
+      except getopt.GetoptError as err:
+        print ('\nErr Syntax Error')
+        usage()
         self.help()
+        sys.exit(2)
       for index, opt in enumerate(args):
         if opt in ("-s", "--seed"):
           #something
-        elif opt in ("-p", "--play"):
-          self.no_marc = False
+          MAKE=False
+        elif opt in ("-p"):
           MAKE = True
         elif opt in ("-h", "--help"):
           self.help()
         elif opt in ("-v", "--verbose"):
+          MAKE=True
         elif index == 0:
           if ( args[0] == 'seed' ):
-            print GREEN +  "\nRun all!!" + NC
+            print (GREEN + "\nRun all!!" + NC)
             self.apps = 'all'
             self.test_all_apps = True
         else:
@@ -47,16 +62,27 @@ def help(self):
           self.help()
       return None
 
-  def initialize(self,argv):
-    self.parseOnlineArgs(argv)
+  def initialize(self,aux):
+    if aux:
+      print ('AUX')
+    
   def main(self,argv):
-    self.initialize(argv)
+    global AUX
+    self.parseOnlineArgs(argv)
+    self.initialize(AUX)
+    AUX = 'This is Museru'
+    print (RED + AUX + NC)
+    return None
     #test
   def __init__(self):
-    self.configure = SafeConfigParser() #see self.autotest_ini file
-    self.configure.read(self.autotest_ini)
+    global etydes_ini
+    self.etydes_ini = 'etydes.ini'
+    parser = configparser.ConfigParser() #see self.autotest_ini file
+    #ini_file = parser.read( self.etydes_ini )
+    ini_file = parser.read( './etydes.ini' )
     #paths
-    self.output_path = self.configure.get('PATHS','SELF_PATH') 
+    #pathsis = ini_file['DEFAULT']
+    self.output_path = parser.get('DEFAULT','SELF_PATH')
     #test
 
   
--- README.txt
+++ README.txt
@@ -1,5 +1,5 @@
 git clone https://github.com/Haraldix/Museru.git
-
+c:/Repos/Museru
 push spell:
 git push -v  https://github.com/Haraldix/Museru Museru_master
 salasana kahdesti!
